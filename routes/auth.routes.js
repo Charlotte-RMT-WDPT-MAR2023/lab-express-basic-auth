80f1d983f3c688762503230d486c3d35eec4bc1c
Charlotte Forsdick <c.l.forsdick@gmail.com>
2023-06-10 12:25:36 +0200

    session and log out

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -24,10 +24,10 @@ router.post("/signup", (req, res, next) => {
       return User.create({
         // username: username
         username,
-                // passwordHash => this is the key from the User model
+        // passwordHash => this is the key from the User model
         //     ^
         //     |            |--> this is placeholder (how we named returning value from the previous method (.hash()))
-        passwordHash: hashedPassword
+        passwordHash: hashedPassword,
       });
     })
     .then((userFromDB) => {
@@ -37,6 +37,58 @@ router.post("/signup", (req, res, next) => {
     .catch((error) => next(error));
 });
 
-router.get("/userProfile", (req, res) => res.render("users/user-profile"));
+router.get("/login", (req, res) => res.render("auth/login"));
+
+router.post("/login", (req, res, next) => {
+  console.log("SESSION =====> ", req.session);
+
+  const { username, password } = req.body;
+
+  if (username === "" || password === "") {
+    res.render("auth/login", {
+      errorMessage: "Please enter both username and password to login.",
+    });
+
+    return;
+  }
+
+  User.findOne({ username })
+
+    .then((user) => {
+      if (!user) {
+        res.render("auth/login", {
+          errorMessage: "Username is not registered. Try with other Username.",
+        });
+
+        return;
+      } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+        // when we introduce session, the following line gets replaced with what follows:
+
+        // res.render('users/user-profile', { user });
+
+        //******* SAVE THE USER IN THE SESSION ********//
+
+        req.session.currentUser = user;
+
+        res.redirect("/userProfile");
+      } else {
+        res.render("auth/login", { errorMessage: "Incorrect password." });
+      }
+    })
+
+    .catch((error) => next(error));
+});
+
+router.get("/userProfile", (req, res) => {
+  res.render("users/user-profile", { userInSession: req.session.currentUser });
+});
+
+router.post("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) next(err);
+
+    res.redirect("/");
+  });
+});
 
 module.exports = router;
